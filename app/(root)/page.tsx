799a2c6086b5db2a39f79b9df3a7ebeeeece1a58
elviredev <sandrine.rodriguez@hotmail.com>
2025-05-05 11:49:26 +0200

    ✨ Live API  & Real-Time Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import StartupCard, { type StartupTypeCard } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
-import { client } from '@/sanity/lib/client';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query
+  const params = { search: query || null }
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
   return (
     <>
@@ -35,6 +36,8 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
